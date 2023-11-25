adc73eb3750fb5c4ad198be5f83d151496520212
SonyGadhre <92802490+SonyGadhre@users.noreply.github.com>
2023-11-26 01:18:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+for(var i=0; i<23;i++){
+    const btn = document.querySelectorAll("button")[i];
+    btn.addEventListener("click",()=>{
+        btn.style.backgroundColor = "#29AB87";
+    }
+)}
+
